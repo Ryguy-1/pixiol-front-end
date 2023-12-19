25153aa2f16ae5493254f0586c09ef5a1840de98
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-18 20:25:49 -0600

    got long articles working

---
+++ app/(home-page)/_components/LongArticle.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { NewsArticle } from "@/api/data-structures";
+import ArticleDuration from "./ArticleDuration";
+import Date from "./Date";
+import ArticleCategoryTag from "./ArticleCategoryTag";
+import Link from "next/link";
+
+interface LongArticleProps {
+  newsArticle?: NewsArticle;
+}
+
+const LongArticle: React.FC<LongArticleProps> = ({ newsArticle }) => {
+  if (!newsArticle) {
+    return (
+      <article className="flex flex-col md:flex-row gap-4">
+        <div className="flex flex-col gap-2 md:w-4/6">
+          <div className="w-2/3 h-[2rem] rounded-xl animate-pulse bg-gray-600" />
+          <div className="w-5/6 h-[4rem] rounded-xl animate-pulse bg-gray-600" />
+          <div className="flex flex-row gap-3 pt-3 flex-wrap">
+            <div className="w-1/6 h-[2rem] rounded-xl animate-pulse bg-gray-600" />
+            <div className="w-1/6 h-[2rem] rounded-xl animate-pulse bg-gray-600" />
+          </div>
+        </div>
+        <div className="flex flex-row md:flex-col justify-start items-start gap-2 md:w-1/6 flex-wrap">
+          <div className="w-1/6 md:w-5/6 h-[2.5rem] rounded-xl animate-pulse bg-gray-600" />
+          <div className="w-1/6 md:w-5/6 h-[2.5rem] rounded-xl animate-pulse bg-gray-600" />
+          <div className="w-1/6 md:w-5/6 h-[2.5rem] rounded-xl animate-pulse bg-gray-600" />
+        </div>
+        <div className="h-[10rem] rounded-3xl md:w-2/6 hidden md:block animate-pulse bg-gray-600" />
+      </article>
+    );
+  }
+
+  const { id, title, content, imageUrl, publishDateStr, minRead, tags } =
+    newsArticle;
+  const CONTENT_PREVIEW_LENGTH = 145;
+
+  return (
+    <article className="flex flex-col md:flex-row gap-4">
+      <div className="flex flex-col gap-2 md:w-4/6">
+        <Link href="/" className="text-4xl font-black hover:underline">
+          {title}
+        </Link>
+        <p>{content.slice(0, CONTENT_PREVIEW_LENGTH)}...</p>
+        <div className="flex flex-row gap-3 pt-3 flex-wrap">
+          {tags.map((tag) => (
+            <ArticleCategoryTag key={tag} category={tag} />
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-row md:flex-col justify-start items-start gap-4 md:w-1/6 flex-wrap">
+        <Date dateString={publishDateStr} />
+        <ArticleDuration durationMinutes={minRead} />
+        <div className="flex flex-row shrink-0">
+          <button className="hover:opacity-50">
+            <img src="/link.svg" alt="link" />
+          </button>
+        </div>
+      </div>
+
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="bg-cover bg-center bg-no-repeat rounded-3xl md:w-2/6 hidden md:block"
+      />
+    </article>
+  );
+};
+
+export default LongArticle;
--- app/(home-page)/page.tsx
+++ app/(home-page)/page.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import {
-  fetchArticleById,
-  fetchMostRecentArticles,
-} from "@/api/_lib/fetchNewsArticles";
+import { fetchMostRecentArticles } from "@/api/_lib/fetchNewsArticles";
 import BigArticle from "./_components/BigArticle";
+import LongArticle from "./_components/LongArticle";
 
 const HomePage: React.FC = async () => {
   const mostRecentArticles = await fetchMostRecentArticles(10);
@@ -13,6 +11,12 @@ const HomePage: React.FC = async () => {
       <div className="flex flex-col justify-center w-[75rem] max-w-[75rem] p-4">
         <section className="grid md:grid-cols-2 grid-cols-1 gap-4">
           <BigArticle newsArticle={mostRecentArticles[0]} />
+          <BigArticle newsArticle={mostRecentArticles[1]} />
+        </section>
+        <div className="border-b-2 border-white my-8"></div>
+        <section className="flex flex-col gap-10">
+          <LongArticle newsArticle={mostRecentArticles[0]} />
+          <LongArticle />
         </section>
         <p>below</p>
       </div>
