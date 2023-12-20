a857a68566c537ad67ae950b41cc537e100bc7cf
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-20 00:17:53 -0600

    removed kbar going to just do it myself

--- app/(navigation)/Navigation.tsx
+++ app/(navigation)/Navigation.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import NavBar from "./_components/NavBar";
 import SideMenu from "./_components/SideMenu";
 import { SideMenuProvider } from "./_components/side-menu-context";
-import ClientKBarProvider from "./_components/ClientKBarProvider";
 
 const Navigation: React.FC = () => {
   return (
     <header>
-      <ClientKBarProvider>
-        <SideMenuProvider startOpen={false}>
-          <NavBar />
-          <SideMenu />
-        </SideMenuProvider>
-      </ClientKBarProvider>
+      <SideMenuProvider startOpen={false}>
+        <NavBar />
+        <SideMenu />
+      </SideMenuProvider>
     </header>
   );
 };
