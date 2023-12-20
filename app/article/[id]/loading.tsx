5172ef3e135aac50976281a6ee44f024378faebb
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-19 20:53:54 -0600

    added article skeleton

---
+++ app/article/[id]/loading.tsx
@@ -0,0 +1,30 @@
+import CenterColumn from "@/_components/CenterColumn";
+import React from "react";
+
+export default function SkeletonArticlePage() {
+  return (
+    <main>
+      <CenterColumn maxWidthRem={60}>
+        <div className="flex flex-col items-start gap-5 lg:pt-20">
+          <div className="h-[4rem] sm:h-[6rem] md:h-[10rem] w-5/6 rounded-xl animate-pulse bg-gray-600"></div>
+          <div className="flex flex-row justify-between w-full">
+            <div className="flex flex-row gap-4 flex-wrap w-5/6">
+              <div className="h-[3rem] w-1/6 rounded-xl animate-pulse bg-gray-600"></div>
+              <div className="h-[3rem] w-1/6 rounded-xl animate-pulse bg-gray-600"></div>
+            </div>
+            <div className="w-1/6">
+              <div className="h-[3rem] w-1/2 rounded-xl animate-pulse bg-gray-600"></div>
+            </div>
+          </div>
+          <div className="flex flex-row gap-4 flex-wrap w-full">
+            <div className="h-[2rem] w-1/6 rounded-xl animate-pulse bg-gray-600"></div>
+            <div className="h-[2rem] w-1/6 rounded-xl animate-pulse bg-gray-600"></div>
+            <div className="h-[2rem] w-1/6 rounded-xl animate-pulse bg-gray-600"></div>
+          </div>
+          <div className="w-full h-[15rem] sm:h-[20rem] md:h-[35rem] rounded-xl animate-pulse bg-gray-600"></div>
+          <div className="h-[3rem] w-full rounded-xl animate-pulse bg-gray-600"></div>
+        </div>
+      </CenterColumn>
+    </main>
+  );
+}
