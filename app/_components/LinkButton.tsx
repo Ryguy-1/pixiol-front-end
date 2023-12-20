d97bd96fcae6dea2a782deee7a50438cd3e0c766
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-19 20:39:47 -0600

    made link button have copied tooltip on click :)

--- app/_components/LinkButton.tsx
+++ app/_components/LinkButton.tsx
@@ -1,19 +1,41 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 interface LinkButtonProps {
   href: string;
 }
 
 const LinkButton: React.FC<LinkButtonProps> = ({ href }) => {
+  const [isTooltipVisible, setTooltipVisible] = useState(false);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    if (isTooltipVisible) {
+      timeout = setTimeout(() => {
+        setTooltipVisible(false);
+      }, 2000);
+    }
+    return () => clearTimeout(timeout);
+  }, [isTooltipVisible]);
+
+  const handleButtonClick = () => {
+    navigator.clipboard.writeText(href);
+    setTooltipVisible(true);
+  };
+
   return (
-    <div className="flex flex-row shrink-0">
-      <button
-        className="hover:opacity-50 shrink-0"
-        onClick={() => navigator.clipboard.writeText(href)}
-      >
+    <div className="flex flex-row shrink-0 relative">
+      <button className="hover:opacity-50 shrink-0" onClick={handleButtonClick}>
         <img src="/link.svg" alt="link" />
       </button>
+      <div
+        className={`absolute top-10 right-0 ml-2 p-2 bg-white rounded text-black font-bold italic text-xs
+         transition-opacity ease-in-out duration-500 ${
+           isTooltipVisible ? "opacity-100" : "opacity-0"
+         }`}
+      >
+        Copied!
+      </div>
     </div>
   );
 };
