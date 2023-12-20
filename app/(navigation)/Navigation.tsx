05fe1d31c4bf7defc88a1befd50ebb92cc618472
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-19 23:35:20 -0600

    adding kbar search

--- app/(navigation)/Navigation.tsx
+++ app/(navigation)/Navigation.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import NavBar from "./_components/NavBar";
 import SideMenu from "./_components/SideMenu";
 import { SideMenuProvider } from "./_components/side-menu-context";
+import ClientKBarProvider from "./_components/ClientKBarProvider";
 
 const Navigation: React.FC = () => {
   return (
     <header>
-      <SideMenuProvider startOpen={false}>
-        <NavBar />
-        <SideMenu />
-      </SideMenuProvider>
+      <ClientKBarProvider>
+        <SideMenuProvider startOpen={false}>
+          <NavBar />
+          <SideMenu />
+        </SideMenuProvider>
+      </ClientKBarProvider>
     </header>
   );
 };
