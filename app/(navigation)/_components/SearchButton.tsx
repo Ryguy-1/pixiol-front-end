f78830e646db9c4804a1ec8cae2779d266c02ba2
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-21 21:04:56 -0700

    adding commands

--- app/(navigation)/_components/SearchButton.tsx
+++ app/(navigation)/_components/SearchButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useCommandPalette } from "./command-pallet-context";
 
 const SearchButton: React.FC = () => {
-  const [isOpen, setIsOpen] = useCommandPalette();
+  const { isOpen, setIsOpen } = useCommandPalette();
   const toggleIsOpen = () => setIsOpen(!isOpen);
   return (
     <button className="hover:opacity-50 shrink-0" onClick={toggleIsOpen}>
--- app/(navigation)/_components/command-pallet-context.tsx
+++ app/(navigation)/_components/command-pallet-context.tsx
@@ -1,31 +1,50 @@
 "use client";
-import React from "react";
+import React, { useState, useContext } from "react";
+
+export interface Command {
+  name: string;
+  iconPath: string;
+  action: () => void;
+}
 
 const CommandPaletteContext = React.createContext<
-  [boolean, React.Dispatch<React.SetStateAction<boolean>>] | undefined
+  | {
+      isOpen: boolean;
+      setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+      commands: Command[];
+      setCommands: React.Dispatch<React.SetStateAction<Command[]>>;
+    }
+  | undefined
 >(undefined);
 
 export function CommandPaletteProvider({
   children,
   startOpen = false,
+  initialCommands = [],
 }: {
   children: React.ReactNode;
   startOpen?: boolean;
+  initialCommands?: Command[];
 }): JSX.Element {
-  const [isOpen, setIsOpen] = React.useState<boolean>(startOpen);
+  const [isOpen, setIsOpen] = useState<boolean>(startOpen);
+  const [commands, setCommands] = useState<Command[]>(initialCommands);
+
+  const value = {
+    isOpen,
+    setIsOpen,
+    commands,
+    setCommands,
+  };
 
   return (
-    <CommandPaletteContext.Provider value={[isOpen, setIsOpen]}>
+    <CommandPaletteContext.Provider value={value}>
       {children}
     </CommandPaletteContext.Provider>
   );
 }
 
-export function useCommandPalette(): [
-  boolean,
-  React.Dispatch<React.SetStateAction<boolean>>
-] {
-  const context = React.useContext(CommandPaletteContext);
+export function useCommandPalette() {
+  const context = useContext(CommandPaletteContext);
 
   if (context === undefined) {
     throw new Error(
