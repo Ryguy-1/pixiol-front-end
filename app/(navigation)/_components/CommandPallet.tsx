0e0ea893743f56caae770c7a7cafac02c15045cf
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-21 20:49:53 -0700

    removed kbar starting over

--- app/(navigation)/_components/CommandPallet.tsx
+++ app/(navigation)/_components/CommandPallet.tsx
@@ -1,27 +1,10 @@
 "use client";
 import React from "react";
-import { KBarProvider } from "kbar";
 
 const CommandPallet: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const actions = [
-    {
-      id: "blog",
-      name: "Blog",
-      shortcut: ["b"],
-      keywords: "writing words",
-      perform: () => (window.location.pathname = "blog"),
-    },
-    {
-      id: "contact",
-      name: "Contact",
-      shortcut: ["c"],
-      keywords: "email",
-      perform: () => (window.location.pathname = "contact"),
-    },
-  ];
-  return <KBarProvider actions={actions}>{children}</KBarProvider>;
+  return <></>;
 };
 
 export default CommandPallet;
