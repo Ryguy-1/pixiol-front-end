21a74a063a63be0bef95dc26a9b7c05b9e04bc7c
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-23 21:03:49 -0700

    made it index categories again because I am going to add them to xml sitemap so it is okay

--- app/category/[categoryId]/page.tsx
+++ app/category/[categoryId]/page.tsx
@@ -35,10 +35,6 @@ export async function generateMetadata(
       url: `${process.env.NEXT_PUBLIC_URL}/category/${params.categoryId}`,
       type: "website",
     },
-    robots: {
-      index: false,
-      follow: false,
-    },
   };
 }
 
