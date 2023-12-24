efa5fb855bc5ec3cb8d512280afa90b6ddc143d3
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-23 21:43:32 -0700

    updated articles to not have lastModified because if updated will not be accurate

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,17 +7,16 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const categories: Category[] = await fetchCategories();
   const articles: NewsArticle[] = await fetchArticles();
 
+  // leave out "lastModified: new Date()"
   return [
     {
       url: "https://www.pixiol.com",
-      // leave out "lastModified: new Date()"
       changeFrequency: "hourly" as const,
       priority: 1,
     },
     ...categories.map((category) => {
       return {
         url: `https://www.pixiol.com/category/${category.id}`,
-        // leave out "lastModified: new Date()"
         changeFrequency: "hourly" as const,
         priority: 0.8,
       };
@@ -25,7 +24,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     ...articles.map((article) => {
       return {
         url: `https://www.pixiol.com/article/${article.id}`,
-        lastModified: new Date(article.publishDateStr),
         changeFrequency: "daily" as const,
         priority: 0.5,
       };
