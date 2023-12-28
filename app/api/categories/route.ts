b33042babc652a23b6dc997224449356d15fd62b
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-27 20:35:51 -0500

    add no cache for route (just category route rn)

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -4,6 +4,8 @@ import { ContentfulCategory, client } from "@/api/contentful";
 import { PersistedCategory } from "@/api/data-structures";
 import { extractContentfulCategoryInformation } from "./utils";
 
+export const dynamic = "force-dynamic"; // set no cache
+
 export async function GET(): Promise<Response> {
   const entries: EntryCollection<ContentfulCategory> = await client.getEntries({
     content_type: "category",
