ddd2286a186387bdf7e4ac0b2347e7a3ecb90adb
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2023-12-29 21:01:38 -0600

    add google ads?

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6896213327075440"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body>
         <SpeedInsights />
         <Analytics />
